32c7863863bcc54144058bf78584b052e5b4c2a7
MotorTruck1221 <motortruck1221@protonmail.com>
2024-03-26 10:50:09 -0600

    Tidy modules

--- internal/wisp/utils.go
+++ internal/wisp/utils.go
@@ -1,17 +1,5 @@
 package wisp
 
 import (
-	"github.com/gorilla/websocket"
 	"net/http"
 )
-
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-	Subprotocols: []string{"wisp-v1"},
-}
-
-func HandleUpgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
-	return upgrader.Upgrade(w, r, nil)
-}
--- internal/wisp/wisp.go
+++ internal/wisp/wisp.go
@@ -1,2 +1,10 @@
 package wisp
 
+const (
+    connectType = 0x01
+    dataType = 0x02
+    continueType = 0x03
+    closeType = 0x04
+    tcpType = 0x01
+    udpType = 0x02
+)
